db9cdbfaff55e41cdcb12822a2e1a236a0bf0ec5
Rhi Batstone <60016148+rhi-batstone@users.noreply.github.com>
2024-03-12 15:51:43 -0600

    Add files via upload

---
+++ analysis.R
@@ -0,0 +1,21 @@
+# ROC for all lineup formats, compare ROCs for social media lineups compared to traditional lineups 
+# 
+# d’? 
+#   
+#   Report proportion of lineup decisions 
+# 
+# Confidence: CAC and calibration curves in four bins  
+# 
+# Multilevel analyses of hit rates, false alarm rates, accuracy, and choosing via Generalized linear mixed models GLMMs (lme4) – models include the conditions of interest in the fixed part of the model 
+# 
+# Need to compare models… with contrasts? 
+#   
+#   social media showups (anonymous or non-anonymous) compared to traditional showups 
+# 
+# social media showups (anonymous or non-anonymous) compared to lineups (social media and traditional)  
+# 
+# Social media showups (anonymous) compared to social media show ups (non-anonymous) 
+# 
+# Social media lineups compared to traditional lineups 
+# 
+# Correct for multiple comparisons where appropriate. 
